e57a2e37ddfbfd453e346bfbe7e07fe80715bc62
Sane Aalam <saneraza78692@gmail.com>
2025-06-22 18:25:41 +0530

    create: schema of instant of user

--- src/app.js
+++ src/app.js
@@ -1,13 +1,35 @@
 const express = require("express");
+const connectDB = require("../config/database.js");
+const User = require("../models/user.js");
 const app = express();
-const port = process.env.PORT || 3000;
-
-console.log(port);
+const port = process.env.PORT || 3000 
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
+app.post("/signup", (req, res) => {
+  const user = new User({
+    firstName: "Akshay 2",
+    lastName: "saini",
+    emailId: "akshaysaini@gmail.com",
+    password: "akshaysaini@123",
+    age: 25,
+    gender: "male",
+  });
+
+  user.save();
+  res.send("user added clearly");
 });
+
+
+connectDB()
+  .then(() => {
+    console.log("database connection is established");
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("database can not established");
+  });
