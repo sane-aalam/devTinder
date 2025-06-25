37589a2d6d4a2e16a948b775d4a93f1ee0272837
Sane Aalam <saneraza78692@gmail.com>
2025-06-25 11:12:41 +0530

    update: user.model.js (add vailidation)

--- models/user.js
+++ models/user.js
@@ -1,27 +1,62 @@
 const mongoose = require("mongoose");
-const { Schema } = mongoose;
 
-const UserSchema = new Schema({
-  firstName: {
-    type: String,
-    required:true,
+const userSchema = new mongoose.Schema({
+    firstname: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 4,
+      maxLength: 50,
+    },
+    lastname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!["Male", "Female", "Other"].includes(value)) {
+          throw new Error("gender data is not valid!");
+        }
+      },
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: 18,
+      max: 50,
+    },
+    skills: {
+      type: [String], // e.g., ["JavaScript", "Node.js"]
+      default: [],
+    },
+    photoUrl: {
+      type: String, // store image URL or file path
+      default:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUZr8J_NnQJMD6bl8-AdMIwE0eP_3jOmCv6xL59PRTuwllTH4uiiU-9h0YdR31H2c09jc&usqp=CAU",
+    },
+    about: {
+      type: String,
+      default: "This is a default about of the user!",
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
-const User = mongoose.model("User", UserSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = User;
