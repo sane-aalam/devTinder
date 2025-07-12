da313e3bb51d9d165733212729ce1a725631a192
Sane <saneraza78692@gmail.com>
2025-07-12 17:34:27 +0530

    read: compound index

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -1,7 +1,4 @@
-// step-1 import mongoose
-// step-2 define schema
-// step-3 create model
-// step-4 export the model
+
 
 const mongoose = require("mongoose");
 
@@ -29,9 +26,8 @@ const connectionRequestSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// ConnectionRequest.find({fromUserId: 19199111222281010, toUserId: 1911018332471910})
-// Duplicate Error fix
 
+// compound index
 connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
 
 connectionRequestSchema.pre("save", function (next) {
@@ -49,3 +45,23 @@ const ConnectionRequest = mongoose.model(
 );
 
 module.exports = ConnectionRequest;
+
+
+
+// learning -
+// step-1 import mongoose
+// step-2 define schema
+// step-3 create model
+// step-4 export the model
+
+/*
+const userSchema = new mongoose.Schema({
+  first: String,
+  second: String
+});
+Schema.index({first:1,second:-1})
+These indexes are created when the schema is registered with MongoDB, ensuring efficient querying for the specified fields.
+
+*/
+// ConnectionRequest.find({fromUserId: 19199111222281010, toUserId: 1911018332471910})
+// Duplicate Error fix
